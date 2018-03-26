957958178351e1e2b70148742687081e1125e5e0
MariiaAsieieva <maxnywa@mail.ru>
2018-03-26 15:45:22 +0300

    block reviews has been done

--- js/init.js
+++ js/init.js
@@ -3,3 +3,10 @@ $( function() {
   $( "#tabs" ).tabs();
   $( "#tabs-portfolio" ).tabs();
 } );
+
+// init slick carousel  
+$('.reviews-slider').slick({ 
+dots: true,
+prevArrow: '<div class="slick-prev slick-arrow"><div class="slider-button"><i class="fa fa-chevron-left"></i></div></div>',
+nextArrow: '<div class="slick-next slick-arrow"><div class="slider-button"><i class="fa fa-chevron-right"></i></div></div>',
+});
